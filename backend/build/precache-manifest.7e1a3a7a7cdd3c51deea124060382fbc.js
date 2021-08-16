dd4351f93c30b51c8f6262adbdce62b540fd3ab3
Augusto Shoo <spice.paldron@gmail.com>
2021-08-16 18:52:07 +0300

    Heroku deployment

---
+++ backend/build/precache-manifest.7e1a3a7a7cdd3c51deea124060382fbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b21a6a7f7e12a5dcb44f293e573468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff3b26bc31de38ac05ef",
+    "url": "/static/css/2.380ee345.chunk.css"
+  },
+  {
+    "revision": "bb78e7d17797ef703214",
+    "url": "/static/css/main.a0b27cd2.chunk.css"
+  },
+  {
+    "revision": "ff3b26bc31de38ac05ef",
+    "url": "/static/js/2.25b98d28.chunk.js"
+  },
+  {
+    "revision": "d05d8ef603fbc27e0e0d49adcdd95c92",
+    "url": "/static/js/2.25b98d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb78e7d17797ef703214",
+    "url": "/static/js/main.15faea5c.chunk.js"
+  },
+  {
+    "revision": "e007e9778f59db94f334",
+    "url": "/static/js/runtime-main.e1c21335.js"
+  },
+  {
+    "revision": "09cf1488a87e83653043b7b0c219c810",
+    "url": "/static/media/video_1.09cf1488.mp4"
+  }
+]);
